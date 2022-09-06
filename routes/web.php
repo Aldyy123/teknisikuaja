b28e316247e012e0b04ad5feed91ad8e173f1f0b
Aldyy231 <53711786+Aldyy123@users.noreply.github.com>
2022-09-06 19:18:10 +0700

    Revert "asd"
    This reverts commit 6de5ab7f580bdb55c5e5dfc2b177e193f95f874e.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/questions', function(){
-    return view('pages.questions');
+Route::get('/', function () {
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {

d89c1884d7b892de8e7f3c32c854e1dd88a13a5a
andikatuluspangestu <andikatulusp@gmail.com>
2022-09-08 18:06:05 +0700

    Fix Folder Structure

---
+++ resources/views/partials/landing/cta.blade.php
@@ -0,0 +1,24 @@
+<!-- CTA -->
+<section class="mt-5">
+    <div class="relative h-screen w-full flex items-center justify-center text-center bg-cover bg-center" style="background-image:url(https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80); height: 400px;">
+        <div class="absolute top-0 right-0 bottom-0 left-0 bg-gray-900 opacity-75"></div>
+
+        <main class="px-4 sm:px-6 lg:px-8 z-10">
+            <div class="text-center">
+                <h2 class="text-4xl tracking-tight leading-10 font-medium sm:text-5xl text-white sm:leading-none md:text-6xl">
+                    <span class="text-white-500 font-bold">Butuh Solusi?</span>
+                </h2>
+                <p class="mt-3 text-white sm:mt-5 sm:text-md sm:max-w-xl sm:mx-auto md:mt-5">
+                    Belum menemukan solusi dari masalah elekronik anda jangan khawatir anda bisa bertanya dengan teknisi secara online maupun offline
+                </p>
+                <div class="mt-5 sm:mt-8 sm:flex justify-center">
+                    <div class="rounded-md shadow">
+                        <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-regular rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:px-10">
+                            Tanya Sekarang
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</section>
---
+++ resources/views/partials/landing/posts.blade.php
@@ -0,0 +1,105 @@
+<!-- Artikel Masalah -->
+<section id="masalah">
+    
+    <!-- Judul -->
+    <div class="text-center pt-10" style="padding-top: 70px !important;">
+        <h2 class="text-3xl font-bold ">Masalah Terbaru</h2>
+    </div>
+
+    <div class="container my-12 mx-auto px-4 md:px-12">
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <!-- Masalah 1 -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <article class="overflow-hidden rounded-lg shadow-lg">
+                    <a href="#">
+                        <img alt="Placeholder" class="block h-auto w-full" src="https://www.99.co/blog/indonesia/wp-content/uploads/2021/10/cara-memperbaiki-tv-yang-tidak-ada-gambarnya.jpg">
+                    </a>
+                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                        <h1 class="text-lg font-bold">
+                            Mengatasi TV Semut
+                        </h1>
+                        <p class="text-grey-darker text-sm">
+                            17/8/2022
+                        </p>
+                    </header>
+                    <p class="pl-2 md:p-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda porro pariatur possimus architecto hic ex
+                    </p>
+                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <a class="flex items-center text-red-600 no-underline hover:underline" href="#">
+                            <p class="text-sm">
+                                Baca Selengkapnya
+                            </p>
+                        </a>
+                        <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
+                            <span class="hidden">Like</span>
+                            <i class="fa fa-heart"></i>
+                        </a>
+                    </footer>
+                </article>
+            </div>
+
+            <!-- Masalah 2 -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <article class="overflow-hidden rounded-lg shadow-lg">
+                    <a href="#">
+                        <img alt="Placeholder" class="block h-auto w-full" src="https://dutaserviceac.com/wp-content/uploads/2019/05/Penyebab-Kulkas-Tidak-Dingin.jpg">
+                    </a>
+                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                        <h1 class="text-lg font-bold">
+                            Penyebab Kulkas tidak Dingin
+                        </h1>
+                        <p class="text-grey-darker text-sm">
+                            17/8/2022
+                        </p>
+                    </header>
+                    <p class="pl-2 md:p-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda porro pariatur possimus architecto hic ex
+                    </p>
+                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <a class="flex items-center text-red-600 no-underline hover:underline" href="#">
+                            <p class="text-sm">
+                                Baca Selengkapnya
+                            </p>
+                        </a>
+                        <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
+                            <span class="hidden">Like</span>
+                            <i class="fa fa-heart"></i>
+                        </a>
+                    </footer>
+                </article>
+            </div>
+
+            <!-- Masalah 3 -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <article class="overflow-hidden rounded-lg shadow-lg">
+                    <a href="#">
+                        <img alt="Placeholder" class="block h-auto w-full" src="https://pict.sindonews.net/dyn/620/pena/news/2022/06/13/122/797127/cara-mengatasi-hp-xiaomi-fastboot-cukup-lakukan-2-langkah-ini-tes.jpg">
+                    </a>
+                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                        <h1 class="text-lg font-bold">
+                            Xiaomi Stuck Logo
+                        </h1>
+                        <p class="text-grey-darker text-sm">
+                            17/8/2022
+                        </p>
+                    </header>
+                    <p class="pl-2 md:p-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda porro pariatur possimus architecto hic ex
+                    </p>
+                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <a class="flex items-center text-red-600 no-underline hover:underline" href="#">
+                            <p class="text-sm">
+                                Baca Selengkapnya
+                            </p>
+                        </a>
+                        <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
+                            <span class="hidden">Like</span>
+                            <i class="fa fa-heart"></i>
+                        </a>
+                    </footer>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>

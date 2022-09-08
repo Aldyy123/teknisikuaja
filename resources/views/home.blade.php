6c01e5b5b97387f304e07e25f9e66ea5e075e899
andikatuluspangestu <andikatulusp@gmail.com>
2022-09-08 23:45:39 +0700

    Clean

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-{{-- All Home Landing Page Components --}}
-@section('container')
-  @include('partials.landing.about') 
-  @include('partials.landing.posts')
-  @include('partials.landing.cta')
-@endsection
-{{-- End All Landing Page Components --}}

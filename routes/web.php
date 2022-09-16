718026a61d4d7f6255aefec8b7e6561825d1241b
Aldyy231 <53711786+Aldyy123@users.noreply.github.com>
2022-09-16 17:10:31 +0700

    add blogs in home

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    $blogs = DB::table('blogs')->orderByDesc('created_at')->limit(3)->get();
+    return view('home', [
+        'blogs' => $blogs,
+    ]);
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';

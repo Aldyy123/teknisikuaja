d21469afc1daea80d1d659e33d79487eb81a5198
andikatuluspangestu <andikatulusp@gmail.com>
2022-09-27 09:08:48 +0700

    Add Form for Questions

---
+++ resources/views/form-tanya.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+{{-- All Home Landing Page Components --}}
+@section('container')
+  @include('partials.tanya.form_tanya')
+@endsection
+{{-- End All Landing Page Components --}}

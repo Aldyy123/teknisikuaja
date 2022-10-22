83eac81780c0301c11c3932ab941afb7fdbdf126
Aldyy231 <53711786+Aldyy123@users.noreply.github.com>
2022-10-22 15:17:31 +0700

    tidy folder

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Questions;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    public function index()
+    {
+        $qestions = Questions::paginate(2);
+        return view('tanya', [
+            'questions' => $qestions,
+        ]);
+    }
+
+    public function detailQuestion($id)
+    {
+        $question = Questions::find($id);
+        return view('detailqs', [
+            'question' => $question
+        ]);
+    }
+
+    public function inputUserQuestion()
+    {
+        return view('form-tanya');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,28 @@
 <?php
 
+use App\Http\Controllers\BlogsController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\TechnicianController;
 use App\Models\Biodatas;
 use App\Models\Questions;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $blogs = DB::table('blogs')->orderByDesc('created_at')->limit(3)->get();
-    return view('home', [
-        'title' => 'Teknisiku - Solusi Kerusakan Elektronik',
-        'blogs' => $blogs,
-    ]);
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/tanya', function () {
-    $qestions = Questions::paginate(2);
-    // dd($qestions);
-    return view('tanya', [
-        'questions' => $qestions,
-    ]);
-})->name('questions');
+Route::get('/tanya', [QuestionController::class, 'index'])->name('questions');
 
-Route::get('/tanya/{id}', function ($id) {
-    $question = Questions::find($id);
-    return view('detailqs', [
-        'question' => $question
-    ]);
-})->name('detailqs');
+Route::get('/tanya/{id}', [QuestionController::class, 'detailQuestion'])->name('detailqs');
 
-Route::get('/detail/{id}', function () {
-    return view('post');
-})->name('detail-post');
+Route::get('/detail/{id}', [BlogsController::class, 'detail'])->name('detail-post');
 
-Route::get('/posts', function () {
-    return view('posts');
-});
+Route::get('/posts', [BlogsController::class, 'index']);
 
-Route::get('/cari-teknisi', function () {
-    $users = Biodatas::all()->where('role', '=', 'technician');
-    return view('cari', [
-        'technician' => $users
-    ]);
-})->name('cari-teknisi');
+Route::get('/cari-teknisi', [TechnicianController::class, 'index'])->name('cari-teknisi');
 
-Route::get('/form-question', function () {
-    return view('form-tanya');
-})->name('form-tanya')->middleware(['auth']);
+Route::get('/form-question', [QuestionController::class, 'inputUserQuestion'])->name('form-tanya')->middleware(['auth']);
 
 Route::get('/dashboard', function () {
     return view('layouts.dashboard');

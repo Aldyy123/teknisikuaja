d1ec7a92b5cb60ebca407fd994bc8ec86c2607d1
andikatuluspangestu <andikatulusp@gmail.com>
2022-10-25 20:25:50 +0700

    See more at Changelogs
    Signed-off-by: andikatuluspangestu <andikatulusp@gmail.com>

--- resources/views/components/card-question.blade.php
+++ resources/views/components/card-question.blade.php
@@ -1,7 +1,9 @@
-<a href="{{ route('detailqs', $id) }}" class="my-2 block box-border">
-    <div class="rounded-md bg-red-500 box-content p-2 hover:bg-transparent text-white hover:text-black transition-all hover:border hover:border-gray-400">
-        <div class="flex gap-4 items-center px-3">
-            <img class="rounded-full w-16 h-16" src="{{ $profile }}" alt="{{$username}}">
+<a href="{{ route('detailqs', $id) }}" class="block box-border m-3">
+    <div
+        class="rounded-md border-gray-200 shadow bg-white-500 box-content p-2 hover:bg-transparent text-gray-800 hover:text-black transition-all hover:border hover:border-gray-400">
+        <div class="flex gap-4 items-center px-3 border-gray-400">
+            <img class="rounded-full w-16 h-16" src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                alt="{{ $username }}">
             <div class="flex flex-col">
                 <h3 class="font-nunito font-bold text-xl">{{ $username }}</h3>
                 <p class="font-sans text-sm">2 jam kemudian</p>
@@ -13,8 +15,7 @@
         <div class="px-3 text-sm">
         </div>
         <div class="flex gap-1 px-3 py-1 text-black flex-wrap">
-            <p class="ring-1 bg-red-200 p-1 rounded-lg text-xs">#Komputer</p>
-            <p class="ring-1 bg-red-200 p-1 rounded-lg text-xs">#tv</p>
+            <p class="ring-0 bg-red-200 p-2 rounded-sm text-xs">Televisi</p>
         </div>
     </div>
 </a>
